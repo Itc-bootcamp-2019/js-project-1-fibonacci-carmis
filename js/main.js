67f5b0322001afa4c503294528856d24e0c7de8b
Carmi_S <carmisnli@gmail.com>
2020-03-05 19:14:38 +0200

    fixed issue: false logic when num < 2

--- js/main.js
+++ js/main.js
@@ -1,30 +1,18 @@
-// TODO fix logical errors in calculator 0, 1 are not correct
-// TODO construct a function to handle edge-cases it breaks now -n, 0, 1, abc, etc. 
+// TODO construct a function to handle edge-cases it breaks now -n, abc, etc.
 
-let calculator = {
-    calculateFibonacci: function(num) {
-        let first = 0,
-            second = 1,
-            next = 1;
-        for (let i = 2; i <= num; i++) {
-            next = first + second;
-            first = second;
-            second = next;
-        }
-        return next;
-    }
+function calculateFibonacci(num) {
+  let a = 1,
+    result = 0,
+    temp;
+  // assumning that the sequence begins with 0, else while num <= 0
+  while (num > 0) {
+    temp = a;
+    a = a + result;
+    result = temp;
+    num--;
+  }
+
+  return result;
 }
-
-
 document.getElementById("x").innerText = "6";
-document.getElementById("y").innerText = calculator.calculateFibonacci(6);
-
-
-// An optional solution intended for pandas style sulution
-// function calculateFibonacci(num) {
-//     let fiboList = [1, 1]; // creating the entire sequence up to n - not good for huge numbers
-//     for (let i = 2; i < num; i++) {
-//         fiboList[i] = fiboList[i - 1] + fiboList[i - 2];
-//     }
-//     return fiboList[num - 1];
-// }
+document.getElementById("y").innerText = calculateFibonacci(6);
