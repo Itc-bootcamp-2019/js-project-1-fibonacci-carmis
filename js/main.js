78ffebac1c9b457625b140932e2d56c103e8215e
Carmi_S <carmisnli@gmail.com>
2020-03-09 13:09:41 +0200

    added a loading spinner

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
+let startCalculatorOnClick = document.getElementById("start-calculator");
+startCalculatorOnClick.addEventListener("click", getFiboServer);
+
 function getFiboServer() {
+  // toggling spinner
+  document.getElementById("calculated-result").innerText = "";
+  document.getElementById("calculated-result").classList.add("spinner-border");
+
   let url = "http://localhost:5050/fibonacci/";
-  let params =  document.getElementById("input-number").value;
-  
-  fetch(`${url}+${params}`)
+  let params = document.getElementById("input-number").value;
+
+  fetch(`${url}${params}`)
     .then(function(response) {
-      // server response ERROR
-      if (response.status !== 200) {
+      if (!response.ok) {
         console.log(
           "Looks like there was a problem. Status Code: " + response.status
         );
         return;
+      } else {
+        response.json().then(function(data) {
+          document
+            .getElementById("calculated-result")
+            .classList.remove("spinner-border");
+          document.getElementById("calculated-result").innerText = data.result;
+        });
       }
-      // response OK
-      response.json().then(function(data) {
-        document.getElementById("calculated-result").innerText = data.result;
-      });
     })
     .catch(function(err) {
       console.log("Fetch Error :-S", err);
     });
 }
-
-let startCalculatorOnClick = document.getElementById("start-calculator");
-startCalculatorOnClick.addEventListener("click", getFiboServer);
-
 
