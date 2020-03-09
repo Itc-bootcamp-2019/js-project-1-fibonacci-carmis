58a639af5af78521319bbccc8699db1b7748b917
Carmi_S <carmisnli@gmail.com>
2020-03-09 14:15:52 +0200

    added a 42 server error

--- js/main.js
+++ js/main.js
@@ -4,29 +4,36 @@ startCalculatorOnClick.addEventListener("click", getFiboServer);
 function getFiboServer() {
   // toggling spinner
   document.getElementById("calculated-result").innerText = "";
+  document.getElementById("calculated-result").classList.remove("result-42");
   document.getElementById("calculated-result").classList.add("spinner-border");
 
   let url = "http://localhost:5050/fibonacci/";
   let params = document.getElementById("input-number").value;
 
-  fetch(`${url}${params}`)
-    .then(function(response) {
-      if (!response.ok) {
-        console.log(
-          "Looks like there was a problem. Status Code: " + response.status
-        );
+  fetch(`${url}${params}`).then(function(response) {
+    if (!response.ok) {
+      response.text().then(function(text) {
+        console.log(text);
+        document.getElementById("calculated-result").innerText = "";
+        document
+          .getElementById("calculated-result")
+          .classList.replace("spinner-border", "result-42");
+        document.getElementById("calculated-result").innerText = `Server Error: ${text}`;
         return;
-      } else {
-        response.json().then(function(data) {
-          document
-            .getElementById("calculated-result")
-            .classList.remove("spinner-border");
-          document.getElementById("calculated-result").innerText = data.result;
-        });
-      }
-    })
-    .catch(function(err) {
-      console.log("Fetch Error :-S", err);
-    });
-}
+      });
 
+      // return;}
+    } else {
+      response.json().then(function(data) {
+        console.log(data);
+        document
+          .getElementById("calculated-result")
+          .classList.remove("spinner-border");
+        document.getElementById("calculated-result").innerText = data.result;
+      });
+    }
+  });
+  // .catch(function(err) {
+  //   console.log("Fetch Error :-S", err);
+  // });
+}
