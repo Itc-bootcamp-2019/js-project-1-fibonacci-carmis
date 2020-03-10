56d96e06034ae1f50583800b0443ad72618e4b20
Carmi_S <carmisnli@gmail.com>
2020-03-10 22:13:42 +0200

    added results section with a toggling spinner

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ document
   .getElementById("start-calculator")
   .addEventListener("click", getFiboServer);
 document.getElementById("input-number").addEventListener("input", clearResults);
+window.addEventListener("DOMContentLoaded", getPreviousCalculations);
 
 function getFiboServer() {
   let inputNum = document.getElementById("input-number");
   let resultValue = document.getElementById("value");
   let url = "http://localhost:5050/fibonacci/";
   let params = inputNum.value;
-
   if (params > 50) {
     toggleStyle("invalid-input-display", "block", "show");
     toggleStyle("input-number", "font-cust1", "font-cust2");
@@ -47,4 +47,16 @@ function clearResults() {
   toggleStyle("invalid-input-display", "show", "block");
   toggleStyle("value", "result-42", "my-font-result");
   toggleStyle("input-number", "font-cust2", "font-cust1");
+}
+
+function getPreviousCalculations() {
+  toggleSpinner("loading");
+  fetch("http://localhost:5050/getFibonacciResults").then(response => {
+    response.json().then(data => {
+      document.getElementById(
+        "result-list"
+      ).innerHTML = JSON.stringify(data, null, 4);
+      toggleSpinner("loading");
+    });
+  });
 }
